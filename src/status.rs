526974c1747ba45ef4ee99a5b0275d3e46f35b44
Matthias Beyer <mail@beyermatthias.de>
2016-04-02 14:02:59 +0200

    Impl De/Serialize for Status

--- src/status.rs
+++ src/status.rs
@@ -1,3 +1,10 @@
+use serde::Serialize;
+use serde::Serializer;
+use serde::Deserialize;
+use serde::Deserializer;
+use serde::de::Error;
+use serde::de::Visitor;
+
 use std::convert::Into;
 use std::convert::From;
 
@@ -10,32 +17,48 @@ pub enum TaskStatus {
     Recurring
 }
 
-impl TaskStatus {
+impl Serialize for TaskStatus {
 
-    pub fn from_str(s: &str) -> Option<TaskStatus> {
-        match s {
-            "pending"   => Some(TaskStatus::Pending),
-            "deleted"   => Some(TaskStatus::Deleted),
-            "completed" => Some(TaskStatus::Completed),
-            "waiting"   => Some(TaskStatus::Waiting),
-            "recurring" => Some(TaskStatus::Recurring),
-            _           => None,
-        }
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), Error>
+        where S: Serializer
+    {
+        serializer.serialize_str(
+            match self {
+                TaskStatus::Pending   => "pending",
+                TaskStatus::Deleted   => "deleted",
+                TaskStatus::Completed => "completed",
+                TaskStatus::Waiting   => "waiting",
+                TaskStatus::Recurring => "recurring",
+            }
+        )
     }
 
 }
 
-impl Into<String> for TaskStatus {
+impl Deserialize for TaskStatus {
+    fn deserialize<D>(deserializer: &mut D) -> Result<TaskStatus, Error>
+        where D: Deserializer
+    {
+        struct TaskStatusVisitor;
 
-    fn into(self) -> String {
-        String::from(match self {
-            TaskStatus::Pending   => "pending",
-            TaskStatus::Deleted   => "deleted",
-            TaskStatus::Completed => "completed",
-            TaskStatus::Waiting   => "waiting",
-            TaskStatus::Recurring => "recurring",
-        })
+        impl Visitor for TaskStatusVisitor {
+            type Value = TaskStatus;
+
+            fn visit_str<E>(&mut self, value: &str) -> Result<TaskStatus, E>
+                where E: Error
+            {
+                match value {
+                    "pending"   => Ok(TaskStatus::Pending),
+                    "deleted"   => Ok(TaskStatus::Deleted),
+                    "completed" => Ok(TaskStatus::Completed),
+                    "waiting"   => Ok(TaskStatus::Waiting),
+                    "recurring" => Ok(TaskStatus::Recurrin),
+                    _           => Err(Error::custom("expected one of pending, deleted, completed, waiting, recurring")),
+                }
+            }
+        }
+
+        deserializer.deserialize(TaskStatusVisitor)
     }
-
 }
 
