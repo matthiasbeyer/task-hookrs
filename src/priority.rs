f9edb545e213a1dd21649500a79242529b3abedf
Matthias Beyer <mail@beyermatthias.de>
2016-04-02 14:02:52 +0200

    Impl De/Serialize for Priority

--- src/priority.rs
+++ src/priority.rs
@@ -1,32 +1,57 @@
+use serde::Serialize;
+use serde::Serializer;
+use serde::Deserialize;
+use serde::Deserializer;
+use serde::de::Error;
+use serde::de::Visitor;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum TaskPriority {
-    NoPrio,
     Low,
     Medium,
     High,
 }
 
-impl<'a> From<&'a str> for TaskPriority {
 
-    fn from(s: &str) -> TaskPriority {
-        match s {
-            "L" => TaskPriority::Low,
-            "M" => TaskPriority::Medium,
-            "H" => TaskPriority::High,
-            _ => TaskPriority::NoPrio,
+impl Serialize for TaskPriority {
+
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+        where S: Serializer
+    {
+        serializer.serialize_str(
+            match self {
+                TaskPriority::Low    => "L",
+                TaskPriority::Medium => "M",
+                TaskPriority::High   => "H",
+            }
+        )
+    }
+
+}
+
+impl Deserialize for TaskPriority {
+
+    fn deserialize<D>(deserializer: &mut D) -> Result<TaskPriority, Error>
+        where D: Deserializer
+    {
+        struct TaskPriorityVisitor;
+
+        impl Visitor for TaskPriorityVisitor {
+            type Value = TaskPriority;
+
+            fn visit_str<E>(&mut self, value: &str) -> Result<TaskPriority, E>
+                where E: Error
+            {
+                match value {
+                    "L" => Ok(TaskPriority::Low),
+                    "M" => Ok(TaskPriority::Medium),
+                    "H" => Ok(TaskPriority::High),
+                    _ => Err(Error::custom("expected one of 'L', 'M', 'H'")),
+                }
+            }
         }
+
+        deserializer.deserialize(TaskPriorityVisitor)
     }
 }
 
-impl Into<String> for TaskPriority {
-
-    fn into(self) -> String {
-        String::from(match self {
-            TaskPriority::Low    => "L",
-            TaskPriority::Medium => "M",
-            TaskPriority::High   => "H",
-            TaskPriority::NoPrio => "",
-        })
-    }
-
-}
