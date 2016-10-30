816d3cbd61f8dde33c8b2c363e404f8095b50e3d
Matthias Beyer <mail@beyermatthias.de>
2016-10-30 18:47:04 +0100

    Impl Deref,DerefMut,Default for UDA

--- src/uda.rs
+++ src/uda.rs
@@ -1,5 +1,9 @@
 //! Module containing the types for User Defined Attributes (UDA)
 
+use std::ops::{Deref, DerefMut};
+use std::default::Default;
+use std::collections::BTreeMap;
+
 /// UDA Name
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
 pub struct UDAName(String);
@@ -36,6 +40,36 @@ impl From<String> for UDAValue {
 
     fn from(s: String) -> UDAValue {
         UDAValue(s)
+    }
+
+}
+
+/// Wrapper type for BTreeMap<UDAName, UDAValue> so serde does not automatically implement the
+/// Deserialize trait on it.
+#[derive(Clone, Debug)]
+pub struct UDA(BTreeMap<UDAName, UDAValue>);
+
+impl Deref for UDA {
+    type Target = BTreeMap<UDAName, UDAValue>;
+
+    fn deref(&self) -> &BTreeMap<UDAName, UDAValue> {
+        &self.0
+    }
+
+}
+
+impl DerefMut for UDA {
+
+    fn deref_mut(&mut self) -> &mut BTreeMap<UDAName, UDAValue> {
+        &mut self.0
+    }
+
+}
+
+impl Default for UDA {
+
+    fn default() -> UDA {
+        UDA(BTreeMap::new())
     }
 
 }
