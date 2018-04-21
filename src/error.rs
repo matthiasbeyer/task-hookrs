fa1c2acbf6745eeef3c19d4945fe6da3c8c446da
Malte Brandy <malte.brandy@maralorn.de>
2018-04-21 03:13:48 +0200

    Refactor error handling to use error_chain

--- src/error.rs
+++ src/error.rs
@@ -5,83 +5,15 @@
 //
 
 //! Error module, containing error types
-
-use std::error::Error;
-use std::fmt::Error as FmtError;
-use std::clone::Clone;
-use std::fmt::{Display, Formatter};
-
-///
-/// Kind of task error
-///
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum TaskErrorKind {
-    /// Error kind indicating that the JSON parser failed
-    ParserError,
-
-    /// Error kind indicating that the Status of a task is missing
-    NoStatus,
-
-    /// Error kind indicating that the Reader failed to read something
-    ReaderError,
-}
-
-fn store_error_type_as_str(e: &TaskErrorKind) -> &'static str {
-    match e {
-        &TaskErrorKind::ParserError => "Parser Error",
-        &TaskErrorKind::NoStatus => "Task status is missing",
-        &TaskErrorKind::ReaderError => "Reader Error",
-    }
-}
-
-impl Display for TaskErrorKind {
-    fn fmt(&self, fmt: &mut Formatter) -> Result<(), FmtError> {
-        write!(fmt, "{}", store_error_type_as_str(self))?;
-        Ok(())
-    }
-}
-
-///
-/// Task error type
-///
-#[derive(Debug)]
-pub struct TaskError {
-    err_type: TaskErrorKind,
-    cause: Option<Box<Error>>,
-}
-
-impl TaskError {
-    ///
-    /// Build a new TaskError from an TaskErrorKind, optionally with cause
-    ///
-    pub fn new(errtype: TaskErrorKind, cause: Option<Box<Error>>) -> TaskError {
-        TaskError {
-            err_type: errtype,
-            cause: cause,
+error_chain!{
+    errors {
+        /// Error kind indicating that the JSON parser failed
+        ParserError {}
+        /// Error kind indicating that the Status of a task is missing
+        NoStatus {
+            description("Task status is missing")
         }
-    }
-
-    ///
-    /// Get the error type of this TaskError
-    ///
-    pub fn err_type(&self) -> TaskErrorKind {
-        self.err_type.clone()
-    }
-}
-
-impl Display for TaskError {
-    fn fmt(&self, fmt: &mut Formatter) -> Result<(), FmtError> {
-        write!(fmt, "[{}]", store_error_type_as_str(&self.err_type.clone()))?;
-        Ok(())
-    }
-}
-
-impl Error for TaskError {
-    fn description(&self) -> &str {
-        store_error_type_as_str(&self.err_type.clone())
-    }
-
-    fn cause(&self) -> Option<&Error> {
-        self.cause.as_ref().map(|e| &**e)
+        /// Error kind indicating that the Reader failed to read something
+        ReaderError {}
     }
 }
