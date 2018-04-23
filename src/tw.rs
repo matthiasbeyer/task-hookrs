804593f7ea7e45f3a04a78a8715410b6ba2201ae
Malte Brandy <malte.brandy@maralorn.de>
2018-04-23 19:32:41 +0200

    Improve Errorhandling to use custom error kinds

--- src/tw.rs
+++ src/tw.rs
@@ -9,7 +9,7 @@
 //! in your path. This will always call task and never interact with your `.task` directory itself.
 //! (This is in accordance with the taskwarrior api guide lines.)
 
-use error::{Result, ResultExt};
+use error::{Result, ResultExt, ErrorKind as EK};
 use task::Task;
 use std::process::{Command, Stdio};
 use std::io::Write;
@@ -24,14 +24,12 @@ pub fn query(query: &str) -> Result<Vec<Task>> {
         cmd.arg(filter);
     }
     let mut export = cmd.arg("export").stdout(Stdio::piped()).spawn().chain_err(
-        || "Failed to start task … export.",
+        || {
+            EK::TaskCmdError
+        },
     )?;
-    export.wait().chain_err(
-        || "Failed to wait for task … export to finish",
-    )?;
-    import(export.stdout.chain_err(
-        || "Failed to capture stdout of task … export",
-    )?)
+    export.wait().chain_err(|| EK::TaskCmdError)?;
+    import(export.stdout.chain_err(|| EK::TaskCmdError)?)
 }
 
 /// This will save the given tasks to taskwarrior. Call with `Some(task)` if you just have one
@@ -40,21 +38,21 @@ pub fn save<T>(tasks: T) -> Result<()>
 where
     T: IntoIterator<Item = Task>,
 {
-    let import = Command::new("task")
+    let tasks: Vec<Task> = tasks.into_iter().collect();
+    let input_buffer = serde_json::to_string(&tasks).chain_err(
+        || EK::SerializeError,
+    )?;
+    let mut import = Command::new("task")
         .arg("import")
         .stdin(Stdio::piped())
         .spawn()
-        .chain_err(|| "Failed to spawn task … import")?;
+        .chain_err(|| EK::TaskCmdError)?;
     import
         .stdin
-        .chain_err(|| "Failed to grap stdin of task import")?
-        .write_all(
-            serde_json::to_string(&tasks.into_iter().collect::<Vec<_>>())
-                .chain_err(|| "Failed to Serialize")?
-                .as_bytes(),
-        )
-        .chain_err(
-            || "Failed to write serialized tasks to stdin of task import",
-        )?;
+        .as_mut()
+        .chain_err(|| EK::TaskCmdError)?
+        .write_all(input_buffer.as_bytes())
+        .chain_err(|| EK::TaskCmdError)?;
+    import.wait().chain_err(|| EK::TaskCmdError)?;
     Ok(())
 }
